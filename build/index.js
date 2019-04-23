4433b441ef99eeea89e616e7576f21e47c72ea1c
CircleCI <brett@annalytics.co.uk>
2019-04-23 18:28:22 +0100

    remove build files

--- build/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var google_service_1 = require("./providers/google.service");
-var goos = new google_service_1.GoogleService();
-goos.parseResponses();
